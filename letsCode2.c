759125f5d787d17c45d5f67bc634a61e6dc27528
Leozinnzz <leozerathelegend@gmail.com>
2025-10-14 13:33:00 -0300

    Atividade 2 resolvida

--- letsCode2.c
+++ letsCode2.c
@@ -7,10 +7,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     
-	int vect[6]; 
+	int vect[6] = {0}; 
+	int n, i;
+	float percent;
+
+	
+	printf("Quantas vezes vc quer jogar o dado: ");
+	scanf("%d", &n);
+	
+	
+	srand(time(NULL));
+	for(i = 0; i < n; i++) {
+		int valor = 1 + rand() % 6;
+			
+		vect[valor - 1]++;	
+	}
+	
+	for(i = 0; i < 6; i++){
+		 percent =   ((float) vect[i]  / n) * 100;
+		 printf("Face %d: %d vezes %.2f%% \n", i + 1, vect[i], percent);
+	}
+	
+	
+	
+	 
+	
+
 	
 }
 
